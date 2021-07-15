4978c0f33390a75a34ba28bfb0f93bd7109d1d47
RodrigoValle <rodrigocostavalle@gmail.com>
2021-07-15 00:22:23 -0300

    entrega do projeto

---
+++ Data/Collections/Infectado.cs
@@ -0,0 +1,21 @@
+﻿using MongoDB.Driver.GeoJsonObjectModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Api.Data.Collections
+{
+    public class Infectado
+    {
+        public DateTime DatadeNascimento { get; set; }
+        public string Sexo { get; set; }
+        public GeoJson2DGeographicCoordinates Localizacao { get; set; }
+        public Infectado(DateTime dataDeNascimento, string sexo, double latitude, double longitude)
+        {
+            DatadeNascimento = dataDeNascimento;
+            Sexo = sexo;
+            Localizacao = new GeoJson2DGeographicCoordinates(longitude, latitude);
+        }
+    }
+}
